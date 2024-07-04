6d74fa1e2a118fb570ed8aaba43a09a182e3ccde
analyticTalesmith <analyticTalesmith@gmail.com>
2024-07-04 12:57:41 -0400

    headless cms update

--- src/app/postTest/[slug]/page.tsx
+++ src/app/postTest/[slug]/page.tsx
@@ -1,10 +1,18 @@
 import { getPostBySlug, getPostsForStaticNav } from "@/app/utils/posts";
 
+type BlogPageProps = {
+    params: BlogPageParams;
+}
+
+type BlogPageParams = {
+    slug: string;
+};
 export async function generateStaticParams() {
     return getPostsForStaticNav();
 }
 
-export default async function Page({ params }) {
+export default async function Page({ params }:BlogPageProps) {
+    console.log(typeof params)
     const { slug } = params;
 
     const res = await getPostBySlug(slug);
