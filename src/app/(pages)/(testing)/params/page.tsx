3c88bbef0b5c51296667e83ea118e2ba97bea888
analyticTalesmith <analyticTalesmith@gmail.com>
2024-07-19 14:47:59 -0400

    search param test

--- src/app/(pages)/(testing)/params/page.tsx
+++ src/app/(pages)/(testing)/params/page.tsx
@@ -7,13 +7,16 @@ import { Suspense } from 'react';
 function PageQuery() {
     const searchParams = useSearchParams()
 
-    return <input placeholder="Search..." />
+    return (<input placeholder="Search..." />)
 }
 
 export default function SearchBar() {
+    const searchParams = useSearchParams();
+
     <Suspense>
         < PageQuery />
     </Suspense>
+    return (<div>Page: {searchParams.get('page')}</div>)
 
     // URL -> `/dashboard?search=my-project`
     // `search` -> 'my-project'
